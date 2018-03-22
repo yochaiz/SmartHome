23677ab714fc4dfdc4cc51da4fd297f216fb168b
yochaiz <yochaizur@gmail.com>
2018-03-22 10:52:36 +0200

    * improved sbatch script

--- Reinforcement/DDPG/sbatch.sh
+++ Reinforcement/DDPG/sbatch.sh
@@ -4,10 +4,5 @@
 #SBATCH -w giptarshish
 
 source ~/tf/bin/activate # activate Tensorflow virtual environment
-echo "test"
-python train.py 0 --random --k 32 --desc "fixed future action (discrete) + knn over all possible discrete actions" &
-echo "1 is running, going to sleep"
-sleep 5 # sleep for few seconds
-echo "good morning, running 2"
-python train.py 0 --random --desc "fixed future action (discrete) + default knn size"
-echo "2 is running"
+python sbatch.py
+
