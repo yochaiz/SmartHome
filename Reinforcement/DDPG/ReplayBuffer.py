36d4230a79e6e8c0aeb480968ee4e87c0e19c600
yochaiz <yochaizur@gmail.com>
2018-05-06 20:05:05 +0300

    * split memory to 2 groups: good reward & bad reward and sample evenly from groups for training

--- Reinforcement/DDPG/ReplayBuffer.py
+++ Reinforcement/DDPG/ReplayBuffer.py
@@ -1,21 +1,111 @@
 from collections import deque
 import numpy as np
 import random
+import heapq
+
+
+class MaxHeapObj(object):
+    def __init__(self, val):
+        self.val = val
+
+    def __lt__(self, other):
+        return self.val > other.val
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+    def __str__(self):
+        return str(self.val)
+
+
+class MinHeap(object):
+    def __init__(self):
+        self.h = []
+
+    def heappush(self, x):
+        heapq.heappush(self.h, x)
+
+    def heappop(self):
+        return heapq.heappop(self.h)
+
+    def __getitem__(self, i):
+        return self.h[i]
+
+    def __len__(self):
+        return len(self.h)
+
+    def remove(self, v):
+        self.h.remove(v)
+
+    def heaptop(self):
+        if len(self.h) > 0:
+            return self.h[0]
+        else:
+            return None
+
+
+class MaxHeap(MinHeap):
+    def heappush(self, x):
+        heapq.heappush(self.h, MaxHeapObj(x))
+
+    def heappop(self):
+        return heapq.heappop(self.h).val
+
+    def __getitem__(self, i):
+        return self.h[i].val
+
+    def remove(self, v):
+        self.h.remove(MaxHeapObj(v))
 
 
 class ReplayBuffer:
     def __init__(self, size, gamma):
+        # init deque
         self.memory = deque(maxlen=size)
+        # init heaps
+        self.goodReward = MinHeap()
+        self.badReward = MaxHeap()
+
         self.gamma = gamma
 
     def remember(self, state, action, reward, next_state):
-        self.memory.append((state, action, reward, next_state))
+        element = (reward, state, action, next_state)
+
+        if len(self.memory) == self.memory.maxlen:
+            vOut = self.memory[0]
+            # remove oldest deque element from heaps
+            if vOut >= self.goodReward.heaptop():
+                self.goodReward.remove(vOut)
+            else:
+                self.badReward.remove(vOut)
+
+        # add element to the relevant heap
+        if (len(self.goodReward) > 0) and (element >= self.goodReward.heaptop()):
+            self.goodReward.heappush(element)
+        else:
+            self.badReward.heappush(element)
+
+        # balance heaps
+        if abs(len(self.goodReward) - len(self.badReward)) > 1:
+            if len(self.goodReward) > len(self.badReward):
+                vOut = self.goodReward.heappop()
+                self.badReward.heappush(vOut)
+            else:
+                vOut = self.badReward.heappop()
+                self.goodReward.heappush(vOut)
+
+        # add element to deque
+        self.memory.append(element)
 
     def replay(self, actorMainModel, actorTargetModel, actorTrainFunc, actorWolpertingerFunc, actorUpdateEpsilonFunc,
-               criticMainModel,
-               criticTargetModel, criticGradientsFunc, stateNormalizeFunc, updateTargetModelsParams, trainSetSize):
-        # Sample trainSet from the memory
-        trainSet = random.sample(self.memory, min(trainSetSize, len(self.memory)))
+               criticMainModel, criticTargetModel, criticGradientsFunc, stateNormalizeFunc, updateTargetModelsParams,
+               trainSetSize):
+        # Sample trainSet from  memory
+        # trainSet = random.sample(self.memory, min(trainSetSize, len(self.memory)))
+        trainSet = random.sample(self.goodReward.h, min(int(trainSetSize / 2), len(self.goodReward)))
+        tempSet = random.sample(self.badReward.h, min(trainSetSize - len(trainSet), len(self.badReward)))
+        trainSet.extend([t.val for t in tempSet])
+        del tempSet
 
         trainState = []
         trainAction = []
